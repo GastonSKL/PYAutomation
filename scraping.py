9a1a6dbed22fa07db3a688f28772b63fa3c86a67
Gaston Skamlec <gastonlskamlec@gmail.com>
2023-08-01 10:56:05 -0300

    asd

--- scraping.py
+++ scraping.py
@@ -15,14 +15,15 @@ def extract_news(url):
     cnt += ('<b>HN Top Stories:</b>\n' + '<br>' + '-' * 50 +'<br>')
     response = requests.get(url)
     content = response.content
-    soup = bs4.BeautifulSoup(content,'html.parse')
-    for i,tag in enumerate(soup.find_all('td',attrs={'class':'title','yaling':''})):
+    soup = bs4.BeautifulSoup(content,'html.parser')
+    for i,tag in enumerate(soup.find_all('td',attrs={'class':'title','valing':''})):
         cnt += ((str(i+1) + ' :: ' + tag.text + "\n" + '<br>') if tag.text != 'More' else '')
         #print(tag.prettify) #find all('span', attrs={'class':'sistestr}))
         return(cnt)
     
 
-cnt = extract_news('https://news.vcombinator.com/') ;   
+# cnt = extract_news('https://www.google.com/') ;   
+cnt = ""
 content += cnt
 content += ('<br>------<br>')
 content += ('<br><br>End of Message')
@@ -32,16 +33,27 @@ print('Composing Email...')
 
 SERVER = 'smtp.gmail.com'
 PORT = 587
-FROM = 'gastonlskamlec@gmail.com' 
-TO = 'gastonlskamlec@gmail.com'
-PASS = 'Apolo15235506'
+FROM = '************' 
+TO = '**************'
+PASS = '***********'
 
 msg = MIMEMultipart()
 
-msg['Subject'] = "Historias de hackernews!"
+msg['Subject'] = "Historias de hackernews! " + str(now.day) + '/' + str(now.month)+ '/' + str(now.year)
 msg['From'] = FROM
 msg['To'] = TO
 
 msg.attach(MIMEText(content, 'html'))
 
 print('Initializing server...')
+
+server = smtplib.SMTP(SERVER, PORT)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+server.login(FROM, PASS)
+server.sendmail(FROM, TO, msg.as_string())
+
+print('Email sent...')
+
+server.quit()
